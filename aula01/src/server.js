ffb26926fde6e28ccb51435a4951a15d0d75408d
Antonio <antonio.alves@duque.g12.br>
2026-01-15 22:51:01 -0300

    Aula 01

--- aula01/src/server.js
+++ aula01/src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http'
 
-const server = http.createServer(() =>{
-    
+const server = http.createServer((request, response) =>{
+    console.log("Server is running")
+    return response.end('Hello World again')
 })
 
 server.listen(3333)
