40ac6efa391036166551f5618897371b1da5be2f
Antonio <antonio.alves@duque.g12.br>
2026-01-15 23:18:24 -0300

    Aula 02

---
+++ aula02/src/rotas.js
@@ -0,0 +1,18 @@
+import http from 'node:http'
+
+const server = http.createServer((request, response) =>{
+    const {method, url} = request
+    console.log(method, url)
+    
+    if(method === 'GET' && url === '/users'){
+        return response.end('Listagem de usuários')
+    }
+
+    if(method === 'POST' && url === '/users'){
+        return response.end('Criação de usuarios')
+    }
+
+     return response.end('HELLO WORLD!!')
+})
+
+server.listen(3333)
